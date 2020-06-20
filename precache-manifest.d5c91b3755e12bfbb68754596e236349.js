4e60835a592e537a25abdeccc7870a613074b811
brave-aditya <pratapaditya84@gmail.com>
2020-06-20 09:56:36 +0530

    Updates

---
+++ precache-manifest.d5c91b3755e12bfbb68754596e236349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62cbd8683874d43f309bdb7ca8ac28a8",
+    "url": "/my/index.html"
+  },
+  {
+    "revision": "50ce1b9df9c36354a873",
+    "url": "/my/static/css/main.6991d632.chunk.css"
+  },
+  {
+    "revision": "0cd7fa0d796662be3d3b",
+    "url": "/my/static/js/2.94075ca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my/static/js/2.94075ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ce1b9df9c36354a873",
+    "url": "/my/static/js/main.8666b20e.chunk.js"
+  },
+  {
+    "revision": "5c50b8ac1018602327db",
+    "url": "/my/static/js/runtime-main.9c942580.js"
+  }
+]);
